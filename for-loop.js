cf92e463a743678eb67be56b4a6a173adc665548
gentimouton <gentimouton@gmail.com>
2013-01-03 18:58:25 -0800

    Create for-loop.js

---
+++ for-loop.js
@@ -0,0 +1,7 @@
+// from JavaScript Patterns by Stefanov
+
+// if the array does not change when iterated, 
+// there is no need to recompute its length every iteration
+for (var i = 0, max = myarray.length; i < max; i++) {
+    // do something with myarray[i]
+}
