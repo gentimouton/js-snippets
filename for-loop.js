bb4c8081fdeea8046b65aeb4a211e3af66ba64b8
gentimouton <gentimouton@gmail.com>
2013-01-03 19:02:46 -0800

    Update for-loop.js

--- for-loop.js
+++ for-loop.js
@@ -2,6 +2,9 @@
 
 // if the array does not change when iterated, 
 // there is no need to recompute its length every iteration
-for (var i = 0, max = myarray.length; i < max; i++) {
+var i = 0,
+    max,
+    myarray = [];
+for (i = 0, max = myarray.length; i < max; i++) {
     // do something with myarray[i]
 }
